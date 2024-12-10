3ce4e6fa37ce7920303459bc751840970a5afa58
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2024-12-10 17:30:33 +0100

    fix: add placeholders

--- mod.ts
+++ mod.ts
@@ -1,2 +1,4 @@
 export * from "./src/combinations.ts";
 export * from "./src/permutations.ts";
+export * from "./src/product.ts";
+export * from "./src/power_set.ts";
---
+++ src/power_set.ts
@@ -0,0 +1,5 @@
+import { unimplemented } from "@std/assert/unimplemented";
+
+export function powerSet<T>(_iterable: Iterable<T>): Generator<T[]> {
+  unimplemented();
+}
